47d5c139233c8becdb15f8d2d3e914271afe0cf1
Jiajen Yuan <jiajenyuan@Jiajens-Air.true.true>
2019-09-03 22:52:41 +0700

    sep3

--- cypress/integration/examples/ea.spec.js
+++ cypress/integration/examples/ea.spec.js
@@ -7,13 +7,40 @@ describe("Testing of EA App", () => {
         //Visiting website
         cy.visit("http://eaapp.somee.com/");
 
+        //long way of working with Promise (closure)
+        //cy.get("#loginLink").then(($link) => {
+        //    return $link.text();
+        //}).as("linkText");
+        //cy.get("#loginLink").invoke('text').as("linkText");
+        
+        //shorten way of working with promise using invoke
+        cy.get("#loginLink").invoke('text').as("linkText");
+
         cy.contains("Login").click();
+
+        cy.get("@linkText").then(($x) => {
+            expect($x).is.eql("Login");
+        })
+
 
         cy.url().should("include","/Account/Login");
 
         cy.get("#UserName").type("admin");
+
         cy.get("#Password").type("password");
 
-        cy.get(".btn").click();
+        cy.get(".btn").click({force: true});
+
+        //Click employee list
+        cy.contains("Employee List").click();
+
+        //Table
+        cy.get(".table").find("tr").contains("Prashanth").parent().contains("Benefits").click();
+
+        cy.get(".table").find("tr").as("rows");
+
+        cy.get("@rows").then(($row) => {
+            cy.wrap($row).click({multiple: true});
+        })
     })
 })
