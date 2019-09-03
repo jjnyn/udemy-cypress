7c8bf1ade210dccbe22c2514a4cbd446b1a91c1c
Jiajen Yuan <jiajenyuan@Jiajens-Air.true.true>
2019-09-03 21:32:52 +0700

    sep3

---
+++ cypress/integration/examples/ea.spec.js
@@ -0,0 +1,19 @@
+/// <reference types="Cypress" />
+
+describe("Testing of EA App", () => {
+
+    it("Login Application", () => {
+
+        //Visiting website
+        cy.visit("http://eaapp.somee.com/");
+
+        cy.contains("Login").click();
+
+        cy.url().should("include","/Account/Login");
+
+        cy.get("#UserName").type("admin");
+        cy.get("#Password").type("password");
+
+        cy.get(".btn").click();
+    })
+})
