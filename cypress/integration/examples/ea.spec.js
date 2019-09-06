c652ea89bb677000ee1d1d0942c6b594ab03451a
Jiajen Yuan <jiajenyuan@Jiajens-Air.true.true>
2019-09-06 22:14:38 +0700

    sep 6

--- cypress/integration/examples/ea.spec.js
+++ cypress/integration/examples/ea.spec.js
@@ -1,4 +1,5 @@
 /// <reference types="Cypress" />
+import '@percy/cypress';
 
 describe("Testing of EA App", () => {
 
@@ -10,16 +11,20 @@ describe("Testing of EA App", () => {
         //implicit assertion
         //cy.get("[aria-label='jump to slide 2']",{timeout:60000}).should("have.class","ls-nav-active");
 
+        cy.percySnapshot();
         //explicit assertion
         cy.get("[aria-label='jump to slide 2']",{timeout:60000}).should(($x) => {
             expect($x).to.not.be.null;
         });
+        cy.percySnapshot();
     })
     it("Testing EA Site for assertion with hooks", () => {
+        cy.percySnapshot();
         //explicit assertion
         cy.get("[aria-label='jump to slide 2']",{timeout:60000}).should(($x) => {
             expect($x).to.not.be.null;
         });
+        cy.percySnapshot();
     })
 
 })
